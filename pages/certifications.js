e685714ed1910e7fde9e9541e1eb316a2daedcfa
RoyalWeden <roy.weden@yahoo.com>
2022-01-18 07:26:22 -0600

    updated certifications page

--- pages/certifications.js
+++ pages/certifications.js
@@ -13,58 +13,64 @@ import thumbMTASecurity from '../public/images/certs/MTA-Security-Fundamentals-2
 
 const Certifications = () => (
     <Layout title="Certifications">
-        <Section delay={0.1}>
-            <SimpleGrid columns={[1, 2, 2]} gap={6}>
-                <GridItem
-                    title="IBM Certified Associate Developer - Quantum Computation using Qiskit v0.2X"
-                    thumbnail={thumbQiskitDev}
-                    href="https://www.credly.com/badges/e630aa98-80a9-4b4e-b768-8d4fb4bb8efd"
-                />
-                <GridItem
-                    title="Microsoft Certified: Azure Data Scientist Associate"
-                    thumbnail={thumbDataScientist}
-                    href="https://www.credly.com/badges/e1f2a9fa-e5ff-49ee-b40a-6d0dd76a4571"
-                />
-            </SimpleGrid>
-        </Section>
+        <Container>
+            <Heading as="h3" fontSize={20} mb={4}>
+                Professional Certifications
+            </Heading>
+            
+            <Section delay={0.1}>
+                <SimpleGrid columns={[1, 2, 2]} gap={6}>
+                    <GridItem
+                        title="IBM Certified Associate Developer - Quantum Computation using Qiskit v0.2X"
+                        thumbnail={thumbQiskitDev}
+                        href="https://www.credly.com/badges/e630aa98-80a9-4b4e-b768-8d4fb4bb8efd"
+                    />
+                    <GridItem
+                        title="Microsoft Certified: Azure Data Scientist Associate"
+                        thumbnail={thumbDataScientist}
+                        href="https://www.credly.com/badges/e1f2a9fa-e5ff-49ee-b40a-6d0dd76a4571"
+                    />
+                </SimpleGrid>
+            </Section>
 
-        <Section delay={0.3}>
-            <SimpleGrid columns={[1, 2, 2]} gap={6}>
-                <GridItem
-                    title="Microsoft Certified: Azure AI Fundamentals"
-                    thumbnail={thumbAzureAI}
-                    href="https://www.credly.com/badges/e52ec58d-5e95-4255-874c-ab507a4e8163"
-                />
-                <GridItem
-                    title="MTA: Security Fundamentals"
-                    thumbnail={thumbMTASecurity}
-                    href="https://www.credly.com/badges/815bdcc4-fb60-4e0b-88d4-4c8730fbc8c9"
-                />
-            </SimpleGrid>
-        </Section>
-        <Section delay={0.5}>
-            <SimpleGrid columns={[1, 2, 2]} gap={6}>
-                <GridItem
-                    title="MTA: Introduction to Programming using Pytohn"
-                    thumbnail={thumbMTAPython}
-                    href="https://michaelkougang.tech/images/MTA-Introduction-to-Programming-using-Python-2019.png"
-                />
-                <GridItem
-                    title="IBM Quantum Challenge - Fall 2021"
-                    thumbnail={thumbFallChall}
-                    href="https://www.credly.com/badges/e6c62d55-68bf-47a0-877c-e36ce0d3c98e"
-                />
-            </SimpleGrid>
-        </Section>
-        <Section delay={0.7}>
-            <SimpleGrid columns={[1, 2, 2]} gap={6}>
-                <GridItem
-                    title="IBM Quantum Challenge - Africa 2021"
-                    thumbnail={thumbAfricaChall}
-                    href="https://www.credly.com/badges/669667c3-0720-48ff-8a6a-d349d750980d"
-                />
-            </SimpleGrid>
-        </Section>
+            <Section delay={0.3}>
+                <SimpleGrid columns={[1, 2, 2]} gap={6}>
+                    <GridItem
+                        title="Microsoft Certified: Azure AI Fundamentals"
+                        thumbnail={thumbAzureAI}
+                        href="https://www.credly.com/badges/e52ec58d-5e95-4255-874c-ab507a4e8163"
+                    />
+                    <GridItem
+                        title="MTA: Security Fundamentals"
+                        thumbnail={thumbMTASecurity}
+                        href="https://www.credly.com/badges/815bdcc4-fb60-4e0b-88d4-4c8730fbc8c9"
+                    />
+                </SimpleGrid>
+            </Section>
+            <Section delay={0.5}>
+                <SimpleGrid columns={[1, 2, 2]} gap={6}>
+                    <GridItem
+                        title="MTA: Introduction to Programming using Pytohn"
+                        thumbnail={thumbMTAPython}
+                        href="https://michaelkougang.tech/images/MTA-Introduction-to-Programming-using-Python-2019.png"
+                    />
+                    <GridItem
+                        title="IBM Quantum Challenge - Fall 2021"
+                        thumbnail={thumbFallChall}
+                        href="https://www.credly.com/badges/e6c62d55-68bf-47a0-877c-e36ce0d3c98e"
+                    />
+                </SimpleGrid>
+            </Section>
+            <Section delay={0.7}>
+                <SimpleGrid columns={[1, 2, 2]} gap={6}>
+                    <GridItem
+                        title="IBM Quantum Challenge - Africa 2021"
+                        thumbnail={thumbAfricaChall}
+                        href="https://www.credly.com/badges/669667c3-0720-48ff-8a6a-d349d750980d"
+                    />
+                </SimpleGrid>
+            </Section>
+        </Container>
     </Layout>
 )
 
