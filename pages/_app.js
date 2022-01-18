93aa6fbde7d5dbce69d89bf3b7a4f180ce1861dc
RoyalWeden <roy.weden@yahoo.com>
2022-01-18 08:47:15 -0600

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,26 @@ import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import theme from '../libs/theme'
 import { AnimatePresence } from 'framer-motion'
+import Head from 'next/head'
 
 const Website = ({ Component, pageProps, router }) => {
     return (
-        <ChakraProvider theme={theme}>
-            <Fonts />
-            <Layout router={router}>
-                <AnimatePresence exitBeforeEnter initial={true}>
-                    <Component {...pageProps} key={router.route} />
-                </AnimatePresence>
-            </Layout>
-        </ChakraProvider>
+        <>
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
+                <link rel="manifest" href="/images/favicon/site.webmanifest" />
+            </Head>
+            <ChakraProvider theme={theme}>
+                <Fonts />
+                <Layout router={router}>
+                    <AnimatePresence exitBeforeEnter initial={true}>
+                        <Component {...pageProps} key={router.route} />
+                    </AnimatePresence>
+                </Layout>
+            </ChakraProvider>
+        </>
     )
 }
 
